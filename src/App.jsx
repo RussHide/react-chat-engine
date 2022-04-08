ed86ec2573deab68b54ec75ffa13bb8a897355ad
Fabian Hide <graadev@gmail.com>
2022-04-07 22:13:49 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { ChatEngine } from 'react-chat-engine'
+import ChatFeed from '../components/ChatFeed'
+import './App.css'
+
+function App() {
+
+  return (
+    <ChatEngine
+     
+
+    />
+  )
+}
+
+export default App
